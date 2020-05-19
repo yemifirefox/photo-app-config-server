a0200ff2dd552d0ba070b2d818ffb2ccb297358d
yemifirefox <yemifirefox@yahoo.com>
2020-05-19 12:15:16 +0100

    Initial Commit

---
+++ src/test/java/com/yemifirefox/photoappconfigserver/PhotoAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yemifirefox.photoappconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
